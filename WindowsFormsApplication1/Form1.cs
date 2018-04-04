cfb76c129bd0a231a76f1ddf4d7580d31d9909d2
陈爽 <bujn1017@qq.com>
2018-04-04 11:30:15 +0800

    56

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -103,6 +103,7 @@ namespace WindowsFormsApplication1
                 int j = 0;
                 var aa = ReadUserDataExDLL(ref o, bx, i, j);
                 var ddsf = "sdf111";
+                var rrr = "sss";
                 //    MyDiskInfo o = new MyDiskInfo();
                 //    o.AdapterID =0;
                 //    o.TargetID = 0;
